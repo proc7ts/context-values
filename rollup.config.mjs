b218fca9b39185fc6c140d2ce0ed0b912c0a6680
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-06-08 13:13:54 +0700

    Upgrade deps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,13 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import { externalModules } from '@run-z/rollup-helpers';
 import path from 'path';
+import { defineConfig } from 'rollup';
 import flatDts from 'rollup-plugin-flat-dts';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import ts from 'rollup-plugin-typescript2';
 import typescript from 'typescript';
 
-export default {
+export default defineConfig({
   input: {
     'context-values': './src/index.ts',
     'context-values.updatable': './src/updatable/index.ts',
@@ -52,4 +53,4 @@ export default {
       }),
     ],
   },
-};
+});
