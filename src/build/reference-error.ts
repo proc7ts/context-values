1c7d10d6b0108bd00beb0b3441b70abd7e66f4e8
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-06-22 12:06:04 +0700

    `CxReferenceError`: Add reason

--- src/build/reference-error.ts
+++ src/build/reference-error.ts
@@ -1,4 +1,4 @@
-import { CxEntry } from '../core/entry';
+import { CxEntry } from '../core';
 
 /**
  * Represents an error when non-existent context value referenced. I.e. when requested context entry has no value.
@@ -11,14 +11,21 @@ export class CxReferenceError extends ReferenceError {
   readonly entry: CxEntry<any>;
 
   /**
+   * Original error reason.
+   */
+  readonly reason: unknown;
+
+  /**
    * Constructs context context reference error.
    *
    * @param entry - Illegally referred context entry.
    * @param message - Arbitrary error message.
+   * @param reason - Original error reason.
    */
-  constructor(entry: CxEntry<any>, message = `The ${String(entry)} has no value`) {
+  constructor(entry: CxEntry<any>, message = `The ${String(entry)} has no value`, reason?: unknown) {
     super(message);
     this.entry = entry;
+    this.reason = reason;
   }
 
 }
