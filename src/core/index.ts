ad2d190f16024b33f2a4193517e1c996b98557a4
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-06-27 16:32:51 +0700

    Precise value requests
    - Add `CxRequest.by` to select value request method.
      E.g. to request only provided values, or only default ones.
    - Add `CxRequest.set` to call with evaluated value result.

--- src/core/index.ts
+++ src/core/index.ts
@@ -7,4 +7,5 @@ export * from './entries';
 export * from './entry';
 export * from './reference-error';
 export * from './request';
+export * from './request-method';
 export * from './values';
--- src/core/request.ts
+++ src/core/request.ts
@@ -1,3 +1,5 @@
+import { CxRequestMethod } from './request-method';
+
 /**
  * Context value request.
  *
@@ -14,6 +16,26 @@ export interface CxRequest<TValue> {
    */
   readonly or?: TValue | null | undefined;
 
+  /**
+   * Request method.
+   *
+   * Specifies how to obtain the value. E.g. it can be used to request only {@link CxRequestMethod.Defaults default}
+   * one.
+   */
+  readonly by?: CxRequestMethod;
+
+  /**
+   * Assigns the value.
+   *
+   * When specified, this method is called right before returning the value from {@link CxValues.get} call. It won't be
+   * called if there is no value to return.
+   *
+   * @param value - The {@link CxEntry.Definition.assign value} or {@link CxEntry.Definition.assignDefault default
+   * value} assigned to entry, or a {@link or fallback} one.
+   * @param by - The request method the value is obtained by.
+   */
+  set?(this: void, value: TValue | null, by: CxRequestMethod): void;
+
 }
 
 export namespace CxRequest {
@@ -21,7 +43,7 @@ export namespace CxRequest {
   /**
    * Context value request with fallback specified.
    *
-   * This can be passed to {@link CxValues.get} method as second parameter.
+   * Can be passed to {@link CxValues.get} method as second parameter.
    *
    * @typeParam TValue - Requested context value type.
    */
@@ -31,6 +53,23 @@ export namespace CxRequest {
      * A fallback value to use if there is no value {@link Definition.assign available} for requested entry.
      */
     readonly or: TValue;
+
+    set?(this: void, value: TValue, by: CxRequestMethod): void;
+
+  }
+
+  /**
+   * Context value request without fallback specified.
+   *
+   * Can be passed to {@link CxValues.get} method as second parameter.
+   *
+   * @typeParam TValue - Requested context value type.
+   */
+  export interface WithoutFallback<TValue> extends CxRequest<TValue> {
+
+    readonly or?: undefined;
+
+    set?(this: void, value: TValue, by: CxRequestMethod): void;
 
   }
 
