565df15099e6eea5475a065d8f184799aba2491b
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-06-30 16:04:09 +0700

    `cxRecent()` utilizes internal state

--- src/entries/recent.entry.ts
+++ src/entries/recent.entry.ts
@@ -1,5 +1,4 @@
-import { CxAsset, CxEntry } from '../core';
-import { CxAsset$Updater$createDefault } from './asset.updater.impl';
+import { CxEntry, cxUnavailable } from '../core';
 
 /**
  * Creates single-valued context entry definer that treats the {@link CxEntry.Target.trackRecentAsset most recent asset}
@@ -8,7 +7,8 @@ import { CxAsset$Updater$createDefault } from './asset.updater.impl';
  * The entry value updated each time the {@link CxEntry.Target.trackRecentAsset most recent asset} changes.
  *
  * @typeParam TValue - Context value asset type.
- * @param byDefault - Creates default entry value used when there are no assets.
+ * @param byDefault - Creates default entry value used when there are no assets. The default value evaluated at most
+ * once per context. When omitted, the default value would be unavailable.
  *
  * @returns New context entry definer.
  */
@@ -16,7 +16,7 @@ export function cxRecent<TValue>(
     {
       byDefault,
     }: {
-      byDefault(this: void, target: CxEntry.Target<TValue>): TValue;
+      byDefault?(this: void, target: CxEntry.Target<TValue>): TValue;
     },
 ): CxEntry.Definer<TValue>;
 
@@ -27,40 +27,79 @@ export function cxRecent<TValue>(
  *
  * @typeParam TValue - Context value type.
  * @typeParam TAsset - Context value asset type.
- * @param createUpdater - Creates the entry value updater. Accepts entry definition target as the only parameter.
- * This method is called at most once per context. The returned updater is then notified when most recent asset changes.
+ * @param create - Creates entry value based on recent asset.
+ * @param byDefault - Creates default entry value used when there are no assets. The default value evaluated at most
+ * once per context. When omitted, the default value would be unavailable.
  *
  * @returns New context entry definer.
  */
 export function cxRecent<TValue, TAsset>(
     {
-      createUpdater,
+      create,
+      byDefault,
     }: {
-      createUpdater(this: void, target: CxEntry.Target<TValue, TAsset>): CxAsset.Updater<TValue, TAsset>;
+      create(this: void, recent: TAsset, target: CxEntry.Target<TValue, TAsset>): TValue;
+      byDefault?(this: void, target: CxEntry.Target<TValue, TAsset>): TValue;
     },
 ): CxEntry.Definer<TValue, TAsset>;
 
-export function cxRecent<TValue, TAsset>(
+/**
+ * Creates single-valued context entry definer with internal state base on {@link CxEntry.Target.trackRecentAsset most
+ * recent asset}.
+ *
+ * The entry value updated each time the {@link CxEntry.Target.trackRecentAsset most recent asset} changes.
+ *
+ * @typeParam TValue - Context value type.
+ * @typeParam TAsset - Context value asset type.
+ * @typeParam TState - Internal state type.
+ * @param create - Creates internal entity state by recent asset.
+ * @param byDefault - Creates default internal entity state when there are no assets. The default state evaluated
+ * at most once per context. When omitted, the default state (and thus the value) would be unavailable.
+ * @param delegate - Creates entity value based on entity state accessor. Such value is created at most once per
+ * context. It is expected to delegate its functionality to internal state. E.g. when the value is a function.
+ *
+ * @returns New context entry definer.
+ */
+export function cxRecent<TValue, TAsset, TState>(
     {
+      create,
       byDefault,
-      createUpdater = CxAsset$Updater$createDefault(byDefault!),
+      delegate,
     }: {
-      byDefault?(this: void, target: CxEntry.Target<TValue, TAsset>): TValue;
-      createUpdater?(this: void, target: CxEntry.Target<TValue, TAsset>): CxAsset.Updater<TValue, TAsset>;
+      create(this: void, recent: TAsset, target: CxEntry.Target<TValue, TAsset>): TState;
+      byDefault?(this: void, target: CxEntry.Target<TValue, TAsset>): TState;
+      delegate(this: void, get: (this: void) => TState, target: CxEntry.Target<TValue, TAsset>): TValue;
+    },
+): CxEntry.Definer<TValue, TAsset>;
+
+export function cxRecent<TValue, TAsset, TState>(
+    {
+      create = cxRecent$create,
+      byDefault,
+      delegate = cxRecent$delegate,
+    }: {
+      create?(this: void, recent: TAsset, target: CxEntry.Target<TValue, TAsset>): TState;
+      byDefault?(this: void, target: CxEntry.Target<TValue, TAsset>): TState;
+      delegate?(this: void, get: (this: void) => TState, target: CxEntry.Target<TValue, TAsset>): TValue;
     },
 ): CxEntry.Definer<TValue, TAsset> {
   return target => {
 
-    const getUpdater = target.lazy(createUpdater);
-    let getValue = (): TValue => {
+    const getDefault = byDefault
+        ? target.lazy(byDefault)
+        : cxUnavailable(target.entry);
+    let getState: () => TState;
+    let getValue = target.lazy(target => {
+      target.trackRecentAsset(evaluated => {
+        getState = evaluated
+            ? () => create(evaluated.asset, target)
+            : getDefault;
+      }).whenOff(reason => {
+        getValue = cxUnavailable(target.entry, undefined, reason);
+      });
 
-      const updater = getUpdater();
-
-      getValue = () => updater.get();
-      target.trackRecentAsset(asset => asset ? updater.set(asset.asset) : updater.reset());
-
-      return getValue();
-    };
+      return delegate(() => getState(), target);
+    });
 
     return {
       assign(assigner) {
@@ -68,4 +107,18 @@ export function cxRecent<TValue, TAsset>(
       },
     };
   };
+}
+
+function cxRecent$create<TValue, TAsset, TState>(
+    recent: TAsset,
+    _target: CxEntry.Target<TValue, TAsset>,
+): TState {
+  return recent as unknown as TState;
+}
+
+function cxRecent$delegate<TValue, TAsset, TState>(
+    get: (this: void) => TState,
+    _target: CxEntry.Target<TValue, TAsset>,
+): TValue {
+  return get() as unknown as TValue;
 }
